c8cfaf6e9fd1001f1cb983e423c1009f8cc98326
Ogglord <oggelito@gmail.com>
2022-01-15 14:26:50 +0100

    ?

--- Day14.cs
+++ Day14.cs
@@ -12,7 +12,7 @@ namespace AdventOfCode
         private static StringBuilder value = new StringBuilder();
         public static void Run()
         {
-            var lines = File.ReadLines("data/day14_test.txt");
+            var lines = File.ReadLines("data/day14.txt");
             value.Append(lines.First());
             foreach (var line in lines.Skip(2))
             {
@@ -20,21 +20,31 @@ namespace AdventOfCode
                 _translation.Add(pair[0], pair[1][0]);
             }
 
-            foreach (var kvp in _translation)
+
+
+            for (int i = 0; i < 10; i++)
             {
-                value = value.Replace(kvp.Key, new string(new[]{kvp.Key[0],kvp.Value,kvp.Key[1]}));
+                var insertions = new Dictionary<int, char>();
+
+                for (int pos = 0; pos < value.Length-1; pos++)
+                {
+                    var pair = value[pos].ToString() + value[pos + 1].ToString();
+                    if (_translation.ContainsKey(pair))
+                        insertions.Add(pos + 1, _translation[pair]);
+                }
+                foreach (var item in insertions.OrderByDescending( (KeyValuePair<int, char> arg) => arg.Key))
+                {
+                    value.Insert(item.Key, item.Value);
+                }
+
             }
 
-            var pairs = new List<string>();
-            var valueArr = value.ToString().ToCharArray();
-            for (var i = 0; i < value.Length - 1; i++)
-            {
-                pairs.Add(new string(new[]{valueArr[i],valueArr[i+1]}));
-            }
-
-            var pairCount = pairs.GroupBy(p => p).OrderByDescending(grp => grp.Count()).ToList();
-            var mostCommon = pairs.First();
-            var leastCommon = pairs.Last();
+            var elements = value.ToString().ToCharArray();
+            var rankedElements = elements.GroupBy(p => p).OrderByDescending(grp => grp.Count()).Select(g => new { Element = g.Key, Count = g.Count()});
+            var mostCommon = rankedElements.First();
+            var leastCommon = rankedElements.Last();
+            var partOne = mostCommon.Count - leastCommon.Count;
+            Console.WriteLine($"Part one: {partOne}");
 
         }
 
